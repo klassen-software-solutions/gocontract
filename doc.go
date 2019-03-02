eb2ece19bd8c210ace8c025cee1cd03218552b59
Steven W. Klassen <klassens@acm.org>
2019-03-01 17:59:02 -0700

    Test change of project structure

--- doc.go
+++ doc.go
@@ -1,12 +1,9 @@
 //
-// gocontract/contract/doc.go
+// gocontract/gocontract/doc.go
 //
 // Created by steve on 2019-03-01.
 // Copyright © 2019 Klassen Software Solutions. All rights reserved.
 // Permission is hereby granted for use under the MIT License (https://opensource.org/licenses/MIT)
 //
 
-// Package contract provides support for "programming by contract" assertions.
-package contract
-
-//go:generate echo Hello Generate!
+package gocontract
