6353287c28ffdd6add74bd2171b765f9d388485d
Steven W. Klassen <klassens@acm.org>
2019-03-01 17:47:44 -0700

    Test checkin to help determine project structure

---
+++ contract/doc.go
@@ -0,0 +1,12 @@
+//
+// gocontract/contract/doc.go
+//
+// Created by steve on 2019-03-01.
+// Copyright © 2019 Klassen Software Solutions. All rights reserved.
+// Permission is hereby granted for use under the MIT License (https://opensource.org/licenses/MIT)
+//
+
+// Package contract provides support for "programming by contract" assertions.
+package contract
+
+//go:generate echo Hello Generate!
