4c4bd8a52eb8ab697ae505d4dc44d7215db322f1
Steven W. Klassen <klassens@acm.org>
2019-03-01 20:31:33 -0700

    Another structure test

--- doc.go
+++
@@ -1,9 +0,0 @@
-//
-// gocontract/gocontract/doc.go
-//
-// Created by steve on 2019-03-01.
-// Copyright © 2019 Klassen Software Solutions. All rights reserved.
-// Permission is hereby granted for use under the MIT License (https://opensource.org/licenses/MIT)
-//
-
-package gocontract
