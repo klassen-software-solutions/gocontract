5b2ba5e4213f5b8eabdc2270af15d3a4ed98f2c4
Steven W. Klassen <klassens@acm.org>
2019-03-02 19:44:04 -0700

    Added the condition checking

---
+++ conditions.go
@@ -0,0 +1,81 @@
+//
+// gocontract/conditions.go
+// gocontract
+//
+// Created by steve on 2019-03-02.
+// Copyright © 2019 Klassen Software Solutions. All rights reserved.
+// Permission is hereby granted for use under the MIT License (https://opensource.org/licenses/MIT).
+//
+
+package gocontract
+
+import (
+	"log"
+	"path/filepath"
+	"runtime"
+)
+
+// Preconditions checks the expression(s) given to see that they are all true. If any fail a suitable
+// error message is logged and the program is halted if os.Exit(1).
+//
+// This method should typically be called near the start of a method, possibly right after the parameter
+// checking. However, that is not a requirement. (In fact Preconditions, Conditions, and Postconditions
+// all do exactly the same thing except for a slight difference in the message that is logged. The
+// three methods are provided instead of one simply as a means of documenting their intent.)
+func Preconditions(result bool, results ...bool) {
+	performConditionChecks("Precondition", result, results)
+}
+
+// Conditions checks the expression(s) given to see that they are all true. If any fail a suitable
+// error message is logged and the program is halted if os.Exit(1).
+//
+// This method should typically not be called near the start or end of a method, however that
+// is not a requirement. (In fact Preconditions, Conditions, and Postconditions
+// all do exactly the same thing except for a slight difference in the message that is logged. The
+// three methods are provided instead of one simply as a means of documenting their intent.)
+func Conditions(result bool, results ...bool) {
+	performConditionChecks("Condition", result, results)
+}
+
+// Postconditions checks the expression(s) given to see that they are all true. If any fail a suitable
+// error message is logged and the program is halted if os.Exit(1).
+//
+// This method should typically be called near the end of a method, possibly right after the parameter
+// checking. However, that is not a requirement. (In fact Preconditions, Conditions, and Postconditions
+// all do exactly the same thing except for a slight difference in the message that is logged. The
+// three methods are provided instead of one simply as a means of documenting their intent.)
+//
+// Typically this is called to check the post-conditions of a successful execution of the function. As
+// such it is generally not run when errors cause an early exit. However, if you wish it to run in
+// such cases, you can add defer before the call.
+func Postconditions(result bool, results ...bool) {
+	performConditionChecks("Postcondition", result, results)
+}
+
+func performConditionChecks(conditionType string, result bool, results []bool) {
+	count := 1 + len(results)
+
+	if !result {
+		reportFailureAndExit(conditionType, 1, count)
+	}
+
+	for idx := range results {
+		if !results[idx] {
+			reportFailureAndExit(conditionType, idx+2, count)
+		}
+	}
+}
+
+func reportFailureAndExit(conditionType string, expressionNumber, expressionCount int) {
+	pc, _, _, ok := runtime.Caller(3)
+	if !ok {
+		log.Fatal("runtime.Caller failed, exiting program")
+	}
+
+	frames := runtime.CallersFrames([]uintptr{pc})
+	frame, _ := frames.Next()
+
+	log.Fatalf("%v failed: expression %v of %v\n   in the method %v in the file %v, line %v\n   Terminating!",
+		conditionType, expressionNumber, expressionCount,
+		filepath.Base(frame.Function), filepath.Base(frame.File), frame.Line)
+}
---
+++ conditions_test.go
@@ -0,0 +1,52 @@
+//
+// gocontract/conditions_test.go
+// gocontract
+//
+// Created by steve on 2019-03-02.
+// Copyright © 2019 Klassen Software Solutions. All rights reserved.
+// Permission is hereby granted for use under the MIT License (https://opensource.org/licenses/MIT).
+//
+
+package gocontract
+
+import "testing"
+
+// Note that the following tests are little more than syntax checks as actually testing a
+// failure would cause the program to exit.
+
+func TestPreconditions(t *testing.T) {
+	Preconditions(true)
+
+	Preconditions(true, true)
+
+	Preconditions(true,
+		myFn(),
+		func() bool { return true }(),
+		func() bool { return myFn() }())
+}
+
+func TestConditions(t *testing.T) {
+	Conditions(true)
+
+	Conditions(true, true)
+
+	Conditions(true,
+		myFn(),
+		func() bool { return true }(),
+		func() bool { return myFn() }())
+}
+
+func TestPostconditions(t *testing.T) {
+	Postconditions(true)
+
+	Postconditions(true, true)
+
+	Postconditions(true,
+		myFn(),
+		func() bool { return true }(),
+		func() bool { return myFn() }())
+}
+
+func myFn() bool {
+	return true
+}
